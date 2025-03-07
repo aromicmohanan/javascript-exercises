bf8207590fb3ad5edde0dbd573558a7ab4045e1f
Aromic Mohanan <201433522+aromicmohanan@users.noreply.github.com>
2025-03-07 13:53:24 +0530

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    let temp = [];
+    array.forEach(element => {
+        if (!args.includes(element)) { temp.push(element) }
+    });
+    return temp;
 };
 
 // Do not edit below this line
