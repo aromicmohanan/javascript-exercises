0157100dc88200e1a2bf8fbfd2bfe4499a3fc0a3
Aromic Mohanan <201433522+aromicmohanan@users.noreply.github.com>
2025-03-07 13:21:19 +0530

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let temp = '';
+    for (i = 0; i < string.length; i++) {
+        temp += string[(string.length - 1) - i];
+    }
+    return temp;
 };
 
 // Do not edit below this line
