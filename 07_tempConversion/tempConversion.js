6c2a3a6c9116c75fadf7d8b0ba291e9b08ad21a4
Aromic Mohanan <201433522+aromicmohanan@users.noreply.github.com>
2025-03-07 14:14:57 +0530

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheit) {
+  return ((farenheit - 32) * 5 / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return (celsius * (9 / 5) + 32)
 };
 
 // Do not edit below this line
