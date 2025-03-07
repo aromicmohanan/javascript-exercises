52bdc3b11d51f387cd07cb8530ab1d2970398cb6
Aromic Mohanan <201433522+aromicmohanan@users.noreply.github.com>
2025-03-07 14:03:14 +0530

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    temp = 0;
+    for (i = num1; i <= num2; i++) {
+        temp += i;
+    }
+    return temp;
 };
 
 // Do not edit below this line
